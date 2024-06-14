0e10b8a6b060ad7c0c5882fba1d0f22d9c498385
Vuongvg <vuongcdt@gmail.com>
2024-06-14 15:05:31 +0700

    update GetSquareDataEmptyUpRowByCell , check processingSquare

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,30 +1,40 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class BoardManager : Singleton<BoardManager>
 {
     #region NewCode
 
-    [SerializeField] private List<SquareData> _squareDatas = new();
-    [SerializeField] private List<Square> _squares = new();
+    [SerializeField] private List<SquareData> squareDatas = new();
+    [SerializeField] private List<Square> squares = new();
 
-    private int boardRow = 5;
-    private int boardCol = 6;
+    private int _boardRow = 5;
+    private int _boardCol = 6;
+    private SquareData _processingSquare;
+
+    private int _count;
+
+    private List<MergerAction> _actionsList = new();
 
     private void Start()
     {
         ResetBoard();
 
-        _squareDatas[1].value = 2;
-        _squareDatas[6].value = 2;
-        _squareDatas[7].value = 2;
-        _squareDatas[8].value = 2;
+        squareDatas[1].value = 2;
+        squareDatas[6].value = 2;
+        squareDatas[7].value = 2;
+        squareDatas[8].value = 2;
 
-        Debug.Log(JsonUtility.ToJson(MergeBlock(new Utils.Cell(1, 1))));
-        Debug.Log(JsonUtility.ToJson(SortBlock(new Utils.Cell(1, 1))));
-        Debug.Log(JsonUtility.ToJson(SortAllBlock()));
+        squareDatas[0].value = 16;
+        squareDatas[2].value = 16;
+
+        squareDatas[12].value = 64;
+
+        MergeBlock(new Utils.Cell(1, 1));
+
+        _actionsList.ForEach(e => { Debug.Log(JsonUtility.ToJson(e)); });
     }
 
     private void Shoot()
@@ -44,41 +54,64 @@ public class BoardManager : Singleton<BoardManager>
 
     private void ResetBoard()
     {
-        for (var y = boardRow; y > 0; y--)
+        for (var y = _boardRow; y > 0; y--)
         {
-            for (var x = 0; x < boardCol; x++)
+            for (var x = 0; x < _boardCol; x++)
             {
-                _squareDatas.Add(new SquareData(
-                    new Utils.Cell(x, boardRow - y),
-                    x + (boardRow - y) * boardCol,
+                squareDatas.Add(new SquareData(
+                    new Utils.Cell(x, _boardRow - y),
+                    x + (_boardRow - y) * _boardCol,
                     0));
             }
         }
     }
 
-    private MergerAction MergeBlock(Utils.Cell cell)
+    private void MergeBlock(Utils.Cell cell)
     {
+        _count++;
+        if (_count > 8)
+        {
+            Debug.Log("stack overflow");
+            return;
+        }
+
         var action = new MergerAction();
-        var countBlockSameValue = 1;
+        var countBlockSameValue = 0;
 
         var cellCheck = GetSquareDataByCell(new Utils.Cell(cell.Column, cell.Row));
+        _processingSquare = cellCheck;
 
         var squareLeft = GetSquareDataByCell(new Utils.Cell(cell.Column - 1, cell.Row));
         var squareUp = GetSquareDataByCell(new Utils.Cell(cell.Column, cell.Row - 1));
         var squareRight = GetSquareDataByCell(new Utils.Cell(cell.Column + 1, cell.Row));
 
-        // Debug.Log($"squareLeft {JsonUtility.ToJson(squareLeft)}");
-        // Debug.Log($"cellCheck {JsonUtility.ToJson(cellCheck)}");
-
         countBlockSameValue = CountBlockSameValue(squareLeft, cellCheck, countBlockSameValue, action);
         countBlockSameValue = CountBlockSameValue(squareUp, cellCheck, countBlockSameValue, action);
         countBlockSameValue = CountBlockSameValue(squareRight, cellCheck, countBlockSameValue, action);
 
-        action.squareTarget = new SquareData(cellCheck.cell, cellCheck.index, cellCheck.value);
-        action.newSquareValue = (int)Mathf.Pow(cellCheck.value, countBlockSameValue);
-        cellCheck.value = action.newSquareValue;
+        if (countBlockSameValue == 0)
+        {
+            SortAllBlock();
+            return;
+        }
 
-        return action;
+        action.squareTarget = new SquareData(cellCheck.cell, cellCheck.index, cellCheck.value);
+
+        var powSquares = cellCheck.value * (int)Mathf.Pow(2, countBlockSameValue);
+
+        action.newSquareValue = powSquares;
+        action.type = ActionType.MergeBlock.ToString();
+
+        _actionsList.Add(action);
+
+        cellCheck.value = powSquares;
+
+        if (squareUp != null)
+        {
+            SortBlock(cellCheck, squareUp);
+        }
+
+        SortAllBlock();
     }
 
     private static int CountBlockSameValue(
@@ -87,7 +120,7 @@ public class BoardManager : Singleton<BoardManager>
         int countBlockSameValue,
         MergerAction action)
     {
-        if (squareData != null && squareData.value == cellCheck.value)
+        if (squareData != null && squareData.value == cellCheck.value && squareData.value > 0)
         {
             action.squareSources.Add(new SquareData(squareData.cell, squareData.index, squareData.value));
             squareData.value = 0;
@@ -97,52 +130,73 @@ public class BoardManager : Singleton<BoardManager>
         return countBlockSameValue;
     }
 
-    private List<MergerAction> SortBlock(Utils.Cell cell)
+    private void SortBlock(SquareData cellCheck, SquareData squareUp)
     {
-        var actionList = new List<MergerAction>();
         var action = new MergerAction();
+
+        action.squareSources.Add(new SquareData(cellCheck.cell, cellCheck.index, cellCheck.value));
+        action.squareTarget = new SquareData(squareUp.cell, squareUp.index, squareUp.value);
+        action.newSquareValue = cellCheck.value;
+        action.type = ActionType.SortBlock.ToString();
+
+        _actionsList.Add(action);
+
+        squareUp.value = cellCheck.value;
+        cellCheck.value = 0;
         
-        var cellCheck = GetSquareDataByCell(new Utils.Cell(cell.Column, cell.Row));
-        var squareUp = GetSquareDataByCell(new Utils.Cell(cell.Column, cell.Row - 1));
-
-        if (squareUp is { value: 0 })
+        if (cellCheck == _processingSquare)
         {
-            action.squareSources.Add(cellCheck);
-            action.squareTarget = squareUp;
-            action.newSquareValue = cellCheck.value;
-
-            squareUp.value = cellCheck.value;
-            cellCheck.value = 0;
-            actionList.Add(action);
-            
-            MergerAction mergerAction = MergeBlock(squareUp.cell);
-            actionList.Add(mergerAction);
+            _processingSquare = squareUp;
         }
-
-        return actionList;
+        else
+        {
+            MergeBlock(_processingSquare.cell);
+        }
+        
+        MergeBlock(squareUp.cell);
     }
 
-    private List<MergerAction> SortAllBlock()
+    private void SortAllBlock()
     {
-        var actionList = new List<MergerAction>();
+        var squareHasEmptyBlocksUpRow = squareDatas.Find(item =>
+            item.value > 0 && squareDatas.Any(squareDownRow =>
+                squareDownRow.index == item.index - _boardCol && squareDownRow.value == 0));
 
-        var emptyBlocks =
-            _squareDatas.Find(item =>
-                item.value == 0
-                && _squareDatas.Any(squareDownRow => squareDownRow.index == item.index + 6 && squareDownRow.value > 0));
-
-        if (emptyBlocks != null)
+        if (squareHasEmptyBlocksUpRow == null)
         {
-            var sortActions = SortBlock(emptyBlocks.cell);
-            actionList.AddRange(sortActions);
+            return;
         }
 
-        return actionList;
+        var squareEmptyUpRow = GetSquareDataEmptyUpRowByCell(squareHasEmptyBlocksUpRow.cell);
+
+        if (squareEmptyUpRow == null)
+        {
+            return;
+        }
+
+        SortBlock(squareHasEmptyBlocksUpRow, squareEmptyUpRow);
+    }
+
+    private SquareData GetSquareDataEmptyUpRowByCell(Utils.Cell cell)
+    {
+        SquareData result = null;
+        for (int cellRow = cell.Row - 1; cellRow >= 0; cellRow--)
+        {
+            var squareDataEmpty = squareDatas.Find(item =>
+                item.cell.Row == cellRow && item.cell.Column == cell.Column && item.value == 0);
+            
+            if (squareDataEmpty == null)
+            {
+                return result;
+            }
+            result = squareDataEmpty;
+        }
+        return result;
     }
 
     private SquareData GetSquareDataByCell(Utils.Cell cell)
     {
-        return _squareDatas.Find(item => item.cell.Row == cell.Row && item.cell.Column == cell.Column);
+        return squareDatas.Find(item => item.cell.Row == cell.Row && item.cell.Column == cell.Column);
     }
 
     #endregion
--- Assets/Scripts/MergerAction.cs
+++ Assets/Scripts/MergerAction.cs
@@ -7,4 +7,11 @@ public class MergerAction
     public List<SquareData> squareSources = new();
     public SquareData squareTarget;
     public int newSquareValue;
+    public string type;
+}
+
+public enum ActionType
+{
+    MergeBlock,
+    SortBlock
 }
