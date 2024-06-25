e9c2858210fe0d291039072025df68667a795e73
Vuongvg <vuongcdt@gmail.com>
2024-06-25 10:49:31 +0700

    update GUI pro supper casual

--- Assets/Scripts/UI/HomeScreen.cs
+++ Assets/Scripts/UI/HomeScreen.cs
@@ -1,9 +1,14 @@
-﻿using ZBase.UnityScreenNavigator.Core.Screens;
+﻿using System;
+using Cysharp.Threading.Tasks;
+using ZBase.UnityScreenNavigator.Core.Screens;
 
 namespace UI
 {
-    public class HomeScreen:Screen
+    public class HomeScreen : Screen
     {
-        
+        // public override UniTask Initialize(Memory<object> args)
+        // {
+        //     return UniTask.CompletedTask;
+        // }
     }
 }
