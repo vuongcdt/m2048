84457fcd8595b602d628f59f04f68487f5026636
Vuongvg <vuongcdt@gmail.com>
2024-06-27 19:17:24 +0700

    update new game

--- Assets/Scripts/UI/GameOverModal.cs
+++ Assets/Scripts/UI/GameOverModal.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,18 +6,18 @@ using ZBase.UnityScreenNavigator.Core.Modals;
 
 namespace UI
 {
-    public class GameOverModal:Modal
+    public class GameOverModal : Modal
     {
         [SerializeField] private Button replayButton;
 
         private UIManager _uiManager;
-        
+
         public override UniTask Initialize(Memory<object> args)
         {
             _uiManager = UIManager.Instance;
             replayButton.onClick.RemoveAllListeners();
             replayButton.onClick.AddListener(OnReplayBtnClick);
-            
+
             return UniTask.CompletedTask;
         }
 
